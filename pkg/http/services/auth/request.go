1531a36337af0b051e6b8807a644c1d32a54ab83
Dathan Vance Pattishall <dathanvp@gmail.com>
2020-10-28 18:06:35 -0700

    refactor auth. There is an interface and requests in one package

---
+++ pkg/http/services/auth/request.go
@@ -0,0 +1,24 @@
+package auth
+
+import "github.com/System-Glitch/goyave/v3/validation"
+
+// Goyave provides a powerful, yet easy way to validate all incoming data, no matter
+// its type or its format, thanks to a large number of validation rules.
+
+// Incoming requests are validated using rules set, which associate rules
+// with each expected field in the request.
+
+// Learn more about validation here: https://system-glitch.github.io/goyave/guide/basics/validation.html
+
+// This is the validation rules for the "/echo" route, which is simply
+// writing the input as a response.
+var (
+	LoginRequest validation.RuleSet = validation.RuleSet{
+		"username": {"required", "string"},
+		"password": {"required", "string"},
+	}
+
+	RefreshRequest validation.RuleSet = validation.RuleSet{
+		"refresh_token": {"required", "string"},
+	}
+)
